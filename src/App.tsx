93311f4b4b44b141918caff51fbcd8aaae0c9639
mshukrun <moshe.shuk@gmail.com>
2022-06-23 19:14:30 +0300

    first draft - hokat

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React, { Fragment } from "react";
 import GlobalStyle from "./globalStyle";
-import Shlach from "./shlach/Shlach";
+import Haparasha from "./haparasha/Haparasha";
 
 const App = () => {
   return (
     <Fragment>
       <GlobalStyle />
-      {/* <Truma /> */}
-      {/* <KiTisha /> */}
-      {/* <KiTishaY /> */}
-      {/* <Pikode /> */}
-      {/* <PikodeY /> */}
-      {/* <Tsav /> */}
-      {/* <Tazriaa /> */}
-      {/* <Pesach /> */}
-      {/* <AcheriMot /> */}
-      {/* <Emoor /> */}
-      {/* <Behokotie /> */}
-      {/* <Naso /> */}
-      <Shlach />
+      <Haparasha />
     </Fragment>
   );
 };
