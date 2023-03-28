935c73259935cc33ed8c8340f376bd40f63f1072
mshukrun <moshe.shuk@gmail.com>
2023-03-28 09:24:07 +0300

    [note] first

--- src/haparasha/Haparasha.tsx
+++ src/haparasha/Haparasha.tsx
@@ -9,33 +9,36 @@ import PageTitle from "./PageTitle";
 import Bad from "./Bad";
 import Parasha from "./Parasha";
 import Sipor from "./Sipor";
-import Magic from "./Magic";
 import Brahot from "./Brahot";
 import Chida from "./Chida";
 import FindWords from "./FindWords";
+import Numbers from "./Numbers";
+import OtherLines from "./OtherLines";
 
 const BodyContainer = styled.div`
   display: grid;
   height: 2800px;
-  width: 1200px;
+  width: 1500px;
   grid-template-rows: 220px //10
-    970px //20
-    500px //30
+    1310px //20
+    200px //30
+    350px //30
     120px //50
-    450px //60
-    250px //70
-    340px //80
-    220px //90
-    300px; //100
+    420px //60
+    350px //70
+    550px //80
+    300px //90
+    350px; //100
   grid-template-columns: 1fr 1fr;
   grid-template-areas: "Title Title" //10
     "Vort Vort  " //20
-    "     Parasha FindWords" //30
+    "  Chida    Yahe " //25
+    "  Parasha    Numbers " //30
     "PageTitle  PageTitle " //50
     "        Brahot   Mahane   " //60
-    "        Brahot   Bad   " //70
-    "        Yahe   Sipor" //80
-    "  Chida Magic   " //90
+    "        Brahot   Azarim   " //70
+    "        Brahot   FindWords" //80
+    "  Bad Sipor   " //90
     " Todot Todot   "; //100
   font-family: "Bona Nova", serif;
   row-gap: 8px;
@@ -54,11 +57,12 @@ const Haparasha = () => {
       <Bad />
       <Parasha />
       <Sipor />
-      <Magic />
       <Brahot />
+      <Numbers />
       <Chida />
       <FindWords />
       <Todot />
+      <OtherLines gridArea="Azarim" num={2} listStyle="none" />
     </BodyContainer>
   );
 };
