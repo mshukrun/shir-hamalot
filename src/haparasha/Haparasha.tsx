6940fdcbdb3beae71b9038dce89949de4a752fb4
mshukrun <moshe.shuk@gmail.com>
2023-05-22 23:51:20 +0300

    91 start

--- src/haparasha/Haparasha.tsx
+++ src/haparasha/Haparasha.tsx
@@ -10,31 +10,31 @@ import Bad from "./Bad";
 import Brahot from "./Brahot";
 import Chida from "./Chida";
 import Magic from "./Magic";
-import Parasha from "./Parasha";
-import Tifzoret from "./Tifzoret";
+import OtherSong from "./OtherSong";
+import OtherLines from "./OtherLines";
 
 const BodyContainer = styled.div`
   display: grid;
   height: 2800px;
   width: 1200px;
   grid-template-rows: 220px //10
-    1125px //20
-    400px //40
+    1350px //20
+    240px //40
     120px //50
-    450px //60
-    250px //70
-    270px //80
-    280px //90
-    300px; //100
+    400px //60
+    480px //70
+    220px //80
+    320px //90
+    290px; //100
   grid-template-columns: 600px 600px;
   grid-template-areas: "Title Title" //10
     "Vort Vort  " //20
-    "  Parasha   Mahane    " //40
+    "   Chida Bilbol     " //40
     "PageTitle  PageTitle " //50
-    "        Brahot   Tifzoret   " //60
-    "        Brahot   Magic   " //70
-    "       Brahot  Chida  " //80
-    "  Bad Yahe    " //90
+    "        Brahot   Bad    " //60
+    "        Brahot   Shiron    " //70
+    "       Yahe  Yahe  " //80
+    "  Mahane Magic     " //90
     " Todot Todot   "; //100
   font-family: "Bona Nova", serif;
   row-gap: 8px;
@@ -53,10 +53,10 @@ const Haparasha = () => {
       <Bad />
       <Brahot />
       <Chida />
-      <Parasha />
       <Magic />
-      <Tifzoret />
       <Todot />
+      <OtherSong gridArea="Shiron" num={1} listStyle="none" />
+      <OtherLines gridArea="Bilbol" num={2} listStyle="none" />
     </BodyContainer>
   );
 };
