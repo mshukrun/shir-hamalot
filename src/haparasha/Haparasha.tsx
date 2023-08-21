266c68bfa7c7ea11adcc688d3feb97369787c4bb
mshukrun <moshe.shuk@gmail.com>
2023-08-21 20:35:46 +0300

    [104] start

--- src/haparasha/Haparasha.tsx
+++ src/haparasha/Haparasha.tsx
@@ -8,9 +8,7 @@ import Todot from "./Todot";
 import PageTitle from "./PageTitle";
 import Bad from "./Bad";
 import Brahot from "./Brahot";
-import Chida from "./Chida";
 import Magic from "./Magic";
-import Tifzoret from "./Tifzoret";
 import Sipor from "./Sipor";
 import Parasha from "./Parasha";
 import OtherLines from "./OtherLines";
@@ -21,28 +19,30 @@ const BodyContainer = styled.div`
   width: 1200px;
   grid-template-rows: 220px //10
     590px //20
-    200px //30
-    470px //35
+    150px //30
+    150px //32
+    420px //35
     250px //40
     120px //50
     310px //60
-    390px //70
-    200px //80
-    120px //80
-    230px //90
+    340px //70
+    250px //80
+    250px //85
+    180px //90
     340px; //100
   grid-template-columns: 600px 600px;
   grid-template-areas: "Title Title" //10
     "Vort Vort  " //20
-    "   Chida Beher     " //30
+    "   Tora Beher     " //30
+    "   Tora Briha     " //32
     "   Hizok Gmara     " //35
-    "   Hidod  Sipor     " //40
+    "   Hidod  Gmara     " //40
     "PageTitle  PageTitle " //50
     "        Brahot   Bad    " //60
     "        Brahot  Mahane     " //70
     "       Brahot  Yahe  " //80
-    "       Brahot  Briha  " //85
-    "  Parasha Magic     " //90
+    "       Parasha  Magic   " //85
+    "  Sipor Sipor     " //90
     " Todot Todot   "; //100
   font-family: "Bona Nova", serif;
   row-gap: 8px;
@@ -60,7 +60,6 @@ const Haparasha = () => {
       <Mahane />
       <Bad />
       <Brahot />
-      <Chida />
       <Magic />
       <Todot />
       <Sipor />
@@ -70,6 +69,7 @@ const Haparasha = () => {
       <OtherLines gridArea="Hizok" num={3} listStyle="none" />
       <OtherLines gridArea="Beher" num={4} listStyle="none" />
       <OtherLines gridArea="Hidod" num={5} listStyle="none" />
+      <OtherLines gridArea="Tora" num={6} listStyle="none" />
     </BodyContainer>
   );
 };
