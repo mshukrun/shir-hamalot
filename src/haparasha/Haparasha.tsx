b96e26a7b434edbf2cd142d0964c172ef1e726ef
mshukrun <moshe.shuk@gmail.com>
2024-01-03 07:49:42 +0200

    [120] final

--- src/haparasha/Haparasha.tsx
+++ src/haparasha/Haparasha.tsx
@@ -3,39 +3,35 @@ import styled from "styled-components";
 import Mahane from "./Mahane";
 import Title from "./Title";
 import Vort from "./Vort";
-import Yahe from "./Yahe";
 import Todot from "./Todot";
 import PageTitle from "./PageTitle";
 import Bad from "./Bad";
-import Brahot from "./Brahot";
-import Magic from "./Magic";
 import OtherLines from "./OtherLines";
 import Chida from "./Chida";
 import Parasha from "./Parasha";
 import PageImage from "./PageImage";
+import Tifzoret from "./Tifzoret";
 
 const BodyContainer = styled.div`
   display: grid;
   height: 2800px;
-  width: 1200px;
+  width: 1100px;
   grid-template-rows: 220px //10
-    1100px //20
-    430px //30
+    1120px //20
+    250px //30
     120px //50
     400px //60
-    200px //70
-    260px //80
-    320px //90
-    350px; //110
-  grid-template-columns: 600px 600px;
+    370px //70
+    480px //90
+    260px; //110
+  grid-template-columns: 550px 550px;
   grid-template-areas: "Title Title" //10
     "Vort Vort  " //20
-    "   Hizok    Bad  " //30
+    "    Parasha    Chida " //30
     "PageTitle  PageTitle " //50
-    "        Gmara    Mahane     " //60
-    "        Gmara   Chida    " //70
-    "        Parasha   Magic    " //80
-    "   Paint    Yahe   " //90
+    "        Hizok    Mahane     " //60
+    "        Gmara   Bad    " //70
+    "   Paint   Tifzoret    " //90
     " Todot Todot   "; //110
   font-family: "Bona Nova", serif;
   row-gap: 8px;
@@ -49,13 +45,12 @@ const Haparasha = () => {
       <Title />
       <PageTitle />
       <Vort />
-      <Yahe />
       <Mahane />
       <Bad />
-      <Magic />
       <Todot />
       <Chida />
       <Parasha />
+      <Tifzoret table={{ left: 50 }} words={{ top: 300 }} />
 
       <OtherLines gridArea="Gmara" num={2} listStyle="none" />
       <OtherLines gridArea="Hizok" num={3} listStyle="none" />
