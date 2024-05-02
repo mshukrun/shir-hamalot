be63a2165eb6ce456065ba9805157d69eb34c5fa
mshukrun <moshe.shuk@gmail.com>
2024-05-02 09:02:20 +0300

    [136] final

--- src/haparasha/Haparasha.tsx
+++ src/haparasha/Haparasha.tsx
@@ -3,14 +3,12 @@ import styled from "styled-components";
 import Mahane from "./Mahane";
 import Title from "./Title";
 import Vort from "./Vort";
-import Yahe from "./Yahe";
 import Todot from "./Todot";
 import PageTitle from "./PageTitle";
 import Bad from "./Bad";
 import OtherLines from "./OtherLines";
 import Chida from "./Chida";
 import Parasha from "./Parasha";
-import PageImage from "./PageImage";
 import Magic from "./Magic";
 
 const BodyContainer = styled.div`
@@ -18,23 +16,21 @@ const BodyContainer = styled.div`
   height: 2800px;
   width: 1200px;
   grid-template-rows: 220px //10
-    1050px //20
-    350px //30
+    900px //20
+    500px //30
     120px //50
-    400px //60
-    250px //80
-    220px //90
+    560px //60
+    370px //90
     250px //100
     300px; //110
   grid-template-columns: 550px 550px;
   grid-template-areas: "Title Title" //10
     "Vort Vort  " //20
-    "        Hizok  Paint      " //30
+    "        Hizok  Bad       " //30
     "PageTitle  PageTitle " //50
-    "    Gmara        Mahane     " //60
-    "   Yahe   Parasha     " //80
-    "     Bad  Chida   " //90
-    "   Tora   Magic    " //100
+    "    Gmara      Parasha       " //60
+    "     Mahane  Magic    " //90
+    "   Tora    Chida   " //100
     " Todot Todot   "; //110
   font-family: "Bona Nova", serif;
   row-gap: 8px;
@@ -48,7 +44,6 @@ const Haparasha = () => {
       <Title />
       <PageTitle />
       <Vort />
-      <Yahe />
       <Mahane />
       <Bad />
       <Todot />
@@ -58,7 +53,6 @@ const Haparasha = () => {
       <OtherLines gridArea="Gmara" num={2} listStyle="none" />
       <OtherLines gridArea="Hizok" num={3} listStyle="none" />
       <OtherLines gridArea="Tora" num={5} listStyle="none" />
-      <PageImage gridArea="Paint" />
     </BodyContainer>
   );
 };
