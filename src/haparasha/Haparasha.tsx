8f23abecb1379455d85eec6234f23c83ba7bf527
mshukrun <moshe.shuk@gmail.com>
2024-09-29 20:13:42 +0300

    [158] final

--- src/haparasha/Haparasha.tsx
+++ src/haparasha/Haparasha.tsx
@@ -12,29 +12,28 @@ import Chida from "./Chida";
 import Parasha from "./Parasha";
 import Tifzoret from "./Tifzoret";
 import OtherTable from "./OtherTable";
+import Magic from "./Magic";
 
 const BodyContainer = styled.div`
   display: grid;
   height: 2800px;
   width: 1200px;
   grid-template-rows: 250px //10
-    800px //20
-    540px //30
+    960px //20
+    250px //30
     120px //50
-    400px //60
-    290px //80
+    480px //60
+    300px //80
     200px //90
-    250px //100
     300px; //110
-  grid-template-columns: 550px 550px;
+  grid-template-columns: 500px 500px;
   grid-template-areas: "Title Title" //10
     "Vort Vort  " //20
-    "        Hizok  Gmara      " //30
+    "        Chida  Tora      " //30
     "PageTitle  PageTitle " //50
-    "  Bad          Mahane     " //60
-    "  Parasha    Tora    " //80
-    "     Chida   Tifzoret  " //90
-    "   Yahe   Tifzoret     " //100
+    "  Bad          Hizok     " //60
+    "  Parasha    Magic    " //80
+    "     Yahe   Yahe  " //90
     " Todot Todot   "; //110
   font-family: "Bona Nova", serif;
   row-gap: 8px;
@@ -49,14 +48,12 @@ const Haparasha = () => {
       <PageTitle />
       <Vort />
       <Yahe />
-      <Mahane />
       <Bad />
       <Todot />
       <Parasha />
-      <Tifzoret table={{ left: 95, top: 70 }} words={{ top: 250 }} />
+      <Magic />
       <Chida />
 
-      <OtherTable gridArea="Gmara" num={2} />
       <OtherLines gridArea="Hizok" num={3} listStyle="none" />
       <OtherLines gridArea="Tora" num={5} listStyle="none" />
     </BodyContainer>
