87a5aa2a08e535037cb3ba6bded51a792486ebf3
mshukrun <moshe.shuk@gmail.com>
2024-12-04 08:41:42 +0200

    [166] final

--- src/haparasha/Haparasha.tsx
+++ src/haparasha/Haparasha.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import Mahane from "./Mahane";
 import Title from "./Title";
 import Vort from "./Vort";
 import Yahe from "./Yahe";
@@ -10,31 +9,28 @@ import Bad from "./Bad";
 import OtherLines from "./OtherLines";
 import Chida from "./Chida";
 import Parasha from "./Parasha";
-import Tifzoret from "./Tifzoret";
-import OtherTable from "./OtherTable";
+import Magic from "./Magic";
 
 const BodyContainer = styled.div`
   display: grid;
   height: 2800px;
   width: 1200px;
   grid-template-rows: 250px //10
-    800px //20
-    540px //30
+    880px //20
+    320px //30
     120px //50
-    400px //60
-    290px //80
-    200px //90
-    250px //100
-    300px; //110
-  grid-template-columns: 550px 550px;
+    270px //60
+    330px //80
+    300px //90
+    330px; //110
+  grid-template-columns: 500px 500px;
   grid-template-areas: "Title Title" //10
     "Vort Vort  " //20
-    "        Hizok  Gmara      " //30
+    "     Hizok     Hizok      " //30
     "PageTitle  PageTitle " //50
-    "  Bad          Mahane     " //60
-    "  Parasha    Tora    " //80
-    "     Chida   Tifzoret  " //90
-    "   Yahe   Tifzoret     " //100
+    "  Bad          Tora     " //60
+    "  Parasha  Magic      " //80
+    "     Chida   Yahe  " //90
     " Todot Todot   "; //110
   font-family: "Bona Nova", serif;
   row-gap: 8px;
@@ -49,14 +45,12 @@ const Haparasha = () => {
       <PageTitle />
       <Vort />
       <Yahe />
-      <Mahane />
       <Bad />
       <Todot />
       <Parasha />
-      <Tifzoret table={{ left: 95, top: 70 }} words={{ top: 250 }} />
+      <Magic />
       <Chida />
 
-      <OtherTable gridArea="Gmara" num={2} />
       <OtherLines gridArea="Hizok" num={3} listStyle="none" />
       <OtherLines gridArea="Tora" num={5} listStyle="none" />
     </BodyContainer>
