252f29fc4fcfc927ac4d274640c1d1f9f9517a29
mshukrun <moshe.shuk@gmail.com>
2025-05-12 22:50:57 +0300

    [190] final

--- src/haparasha/Haparasha.tsx
+++ src/haparasha/Haparasha.tsx
@@ -2,42 +2,38 @@ import React from "react";
 import styled from "styled-components";
 import Mahane from "./Mahane";
 import Title from "./Title";
-import Vort from "./Vort";
 import Yahe from "./Yahe";
 import PageTitle from "./PageTitle";
 import Bad from "./Bad";
 import OtherLines from "./OtherLines";
 import Chida from "./Chida";
 import Parasha from "./Parasha";
-import Magic from "./Magic";
 import Pitaron from "./Pitaron";
 import CommonFrameWithoutTitle from "../CommonFrameWithoutTitle";
 import l10n from "./l10n.json";
-import Tifzoret from "./Tifzoret";
+import VortShir from "./VortShir";
 
 const BodyContainer = styled.div`
   display: grid;
   height: 2800px;
   width: 1200px;
   grid-template-rows: 220px //10
-    830px //20
-    550px //30
+    1120px //20
+    260px //30
     120px //50
-    300px //60
-    250px //80
-    200px //80
-    270px //90
+    500px //60
+    220px //80
+    300px //90
     210px //95
     200px //100
     50px; //110
   grid-template-columns: 550px 550px;
   grid-template-areas: "Title Title" //10
-    "Vort Vort  " //20
-    " Bad    Mahane " //30
+    "VortShir VortShir  " //20
+    " Yahe  Bad " //30
     "PageTitle  PageTitle " //50
-    " Parasha Tora " //60
-    " Yahe Tifzoret " //80
-    "  Chida  Tifzoret " //80
+    " Tora Mahane  " //60
+    " Parasha Chida " //80
     " Hizok  Hizok   " //90
     " FW  RIP   " //95
     " Pitaron Pitaron   " // 100
@@ -54,14 +50,13 @@ const Haparasha = () => {
       <Title />
       <PageTitle />
       <Pitaron />
-      <Vort />
+      <VortShir height={960} width={1150} top={1} />
       <Yahe />
       <Mahane />
       <Bad />
 
       <Parasha />
       <Chida />
-      <Tifzoret />
 
       <OtherLines gridArea="Hizok" num={3} />
       <OtherLines gridArea="Tora" num={5} />
