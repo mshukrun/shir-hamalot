e63d72fa5780fbac566c57d365f838eb65d43ae7
mshukrun <moshe.shuk@gmail.com>
2025-08-20 13:23:11 +0300

    [204] final

--- src/haparasha/Haparasha.tsx
+++ src/haparasha/Haparasha.tsx
@@ -11,29 +11,31 @@ import Parasha from "./Parasha";
 import Pitaron from "./Pitaron";
 import CommonFrameWithoutTitle from "../CommonFrameWithoutTitle";
 import l10n from "./l10n.json";
-import VortShir from "./VortShir";
 import Magic from "./Magic";
+import Vort from "./Vort";
 
 const BodyContainer = styled.div`
   display: grid;
   height: 2800px;
   width: 1200px;
   grid-template-rows: 220px //10
-    1020px //20
-    350px //30
+    800px //20
+    200px //30
+    380px //40
     120px //50
-    280px //60
+    220px //60
     220px //70
-    460px //90
+    480px //90
     220px //95
     250px //100
     50px; //110
   grid-template-columns: 550px 550px;
   grid-template-areas: "Title Title" //10
-    "VortShir VortShir  " //20
-    "   Parasha  Tora  " //30
+    "Vort Vort  " //20
+    "   Parasha Bad   " //30
+    "   Tora Bad   " //40
     "PageTitle  PageTitle " //50
-    " Yahe  Bad  " //60
+    " Yahe  Yahe  " //60
     " Chida Magic   " //70
     "  Hizok Mahane  " //30
     " FW  RIP   " //95
@@ -51,7 +53,7 @@ const Haparasha = () => {
       <Title />
       <PageTitle />
       <Pitaron />
-      <VortShir height={930} width={1150} top={1} />
+      <Vort />
       <Yahe />
       <Mahane />
       <Bad />
