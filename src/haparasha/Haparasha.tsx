ddfe2e3c03daa509c694c6763e50a6b6008bd55f
mshukrun <moshe.shuk@gmail.com>
2026-07-02 09:17:36 +0300

    [244] final

--- src/haparasha/Haparasha.tsx
+++ src/haparasha/Haparasha.tsx
@@ -19,27 +19,23 @@ const BodyContainer = styled.div`
   height: 2800px;
   width: 1200px;
   grid-template-rows: 220px //10
-    1040px //20
+    940px //20
     260px //30
     10px //40
     120px //50
-    330px //60
-    190px //70
-    340px //80
-    80px //90
+    450px //60
+    250px //70
     200px //95
     220px //100
     50px; //110
-  grid-template-columns: 500px 550px;
+  grid-template-columns: 500px 500px;
   grid-template-areas: "Title Title" //10
-    "VortShir VortShir  " //20
-    " Magic    Parasha " //30
+    "Vort Vort  " //20
+    " Magic   Chida  " //30
     "BlankSpace BlankSpace" //40
     "PageTitle  PageTitle " //50
-    " Yahe   Sahir  " //60
-    " Chida  Sahir   " //70
-    "   Tora  Hizok  " //80
-    "   Shiraim  Shiraim  " //90
+    " Hizok   Sahir  " //60
+    " Yahe  Parasha   " //70
     " FW  RIP   " //95
     " Pitaron Pitaron   " // 100
     " Contact Contact "; //110
@@ -55,15 +51,13 @@ const Haparasha = () => {
       <Title />
       <PageTitle />
       <Pitaron />
-      <VortShir height={930} width={1000} />
+      <Vort />
       <Yahe padding={10} />
       <Magic />
-      <Parasha />
       <Chida />
+      <Parasha />
       <OtherLines gridArea="Sahir" num={1} padding={10} />
       <OtherLines gridArea="Hizok" num={3} padding={10} />
-      <OtherLines gridArea="Tora" num={5} />
-      <OtherLines gridArea="Shiraim" num={6} padding={10} />
       <CommonFrameWithoutTitle gridArea="Contact" content={l10n.contact} />
       <CommonFrameWithoutTitle
         gridArea="RIP"
